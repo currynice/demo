0f2309028017e2bcd43400bebc4b255444d02a16
chengxinyu <694975984@qq.com>
2019-07-29 17:38:24 +0800

    exception handler2

---
+++ exception-handler/src/main/java/com/cxy/exceptionhandler/ExceptionHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.cxy.exceptionhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionHandlerApplication.class, args);
+    }
+
+}
