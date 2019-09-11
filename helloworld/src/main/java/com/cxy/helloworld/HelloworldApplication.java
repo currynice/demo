e8c6c4f546e8b13bea5de9c4c2c606dd2af92424
cxy <694975984@qq.com>
2019-09-11 18:03:17 +0800

    1

--- helloworld/src/main/java/com/cxy/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/cxy/helloworld/HelloworldApplication.java
@@ -2,11 +2,22 @@ package com.cxy.helloworld;
 
 import cn.hutool.core.util.StrUtil;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * @SpringBootApplication{
+ * {@link SpringBootConfiguration 相当于 @Configuration ,applicationContext.xml 可以自由配置bean}
+ * {@link EnableAutoConfiguration 开启自动化配置,可以覆写替代}
+ * {@link ComponentScan 完成包扫描}
+ * }
+ */
 @SpringBootApplication
 @RestController
 public class HelloworldApplication {
@@ -27,6 +38,11 @@ public class HelloworldApplication {
     }
 
 
+    //visit like /demo/hello?who=cxy
+    @GetMapping("myError")
+    public String myError(){
+        return "myError";
 
+    }
 
 }
