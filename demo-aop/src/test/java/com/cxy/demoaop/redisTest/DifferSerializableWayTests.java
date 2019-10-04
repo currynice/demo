9a64704882d341c3a71f85397b67204fefed878f
cxy <694975984@qq.com>
2019-10-05 00:51:13 +0800

    10.5

--- demo-aop/src/test/java/com/cxy/demoaop/redisTest/DifferSerializableWayTests.java
+++ demo-aop/src/test/java/com/cxy/demoaop/redisTest/DifferSerializableWayTests.java
@@ -1,7 +1,6 @@
 package com.cxy.demoaop.redisTest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
