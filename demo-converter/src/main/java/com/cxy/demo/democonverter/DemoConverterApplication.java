286a38245438993e2d71cfde259b34a8d49e921e
cxy <694975984@qq.com>
2019-12-17 11:13:17 +0800

    converter

---
+++ demo-converter/src/main/java/com/cxy/demo/democonverter/DemoConverterApplication.java
@@ -0,0 +1,13 @@
+package com.cxy.demo.democonverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoConverterApplication.class, args);
+    }
+
+}
---
+++ demo-converter/src/test/java/com/cxy/demo/democonverter/DemoConverterApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cxy.demo.democonverter;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
