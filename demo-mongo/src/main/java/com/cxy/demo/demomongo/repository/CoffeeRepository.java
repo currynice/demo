97f7dd093a0b49447ecffdcd175b007c12d90b79
cxy <694975984@qq.com>
2020-05-11 17:08:37 +0800

    restful api by using mongoDB

--- demo-mongo/src/main/java/com/cxy/demo/demomongo/repository/CoffeeRepository.java
+++ demo-mongo/src/main/java/com/cxy/demo/demomongo/repository/CoffeeRepository.java
@@ -3,9 +3,12 @@ package com.cxy.demo.demomongo.repository;
 
 import com.cxy.demo.demomongo.entity.Coffee;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+
+@RepositoryRestResource(collectionResourceRel = "coffee",path = "coffee")
 public interface CoffeeRepository extends MongoRepository<Coffee, String> {
     List<Coffee> findByName(String name);
 }
